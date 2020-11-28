b09f92cbf79dfd11ea37ea4e59c5ef1b4d921447
dss13 <s.sumugan007@gmail.com>
2020-11-28 18:30:09 +0530

    remove extra div

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,35 @@
 const hr1 = document.querySelector("#hr1");
-const hr2 = document.querySelector("#hr2");
+const hr2 = document.querySelector("#timeH");
 const min1 = document.querySelector("#min1");
-const min2 = document.querySelector("#min2");
-
+const min2 = document.querySelector("#timeM");
+const card = document.querySelector('#time-card')
 const button = document.querySelector('#calculate');
-
+const display = document.querySelector('#display-time')
+const jTime = document.querySelector('#all-time')
 button.addEventListener('click', (e) => {
     e.preventDefault()
-    const totalMins = parseInt(parseInt((parseInt(hr1.value) + parseInt(hr2.value)) * 60) + parseInt(min1.value) + parseInt(min2.value));
+    card.classList.remove("uk-animation-slide-bottom-small");
+    display.classList.remove("uk-animation-fade")
+
+    const totalMins = parseInt(parseInt((parseInt(hr1.value) + parseInt(hr2.innerText)) * 60) + parseInt(min1.value) + parseInt(min2.innerText));
     const hrs = parseInt(totalMins / 60);
     const mins = parseInt(totalMins % 60);
+    hr2.innerText = hrs;
+    min2.innerText = mins;
+    hr1.value = 0;
+    min1.value = 0;
+    if (hrs < 4) {
+        jTime.classList.remove('uk-text-success')
 
-    document.querySelector('#timeText').innerHTML = `${hrs}h ${mins}m`
+        jTime.classList.add('uk-text-danger')
+    } else {
+        jTime.classList.remove('uk-text-danger')
+
+        jTime.classList.add('uk-text-success')
+    }
+    card.classList.add("uk-animation-slide-bottom-small");
+    display.classList.add("uk-animation-fade")
+
+    // document.querySelector('#timeText').innerHTML = `${hrs}h ${mins}m`
 
 })
